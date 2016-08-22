83972c4bdaec55476fa8b3bc61cfe5a41201aff1
sh14 <sh14@software.nju.edu.cn>
2016-08-22 18:24:49 +0800

    安卓：1. 添加了banner控件

--- FinanceCity_Android/app/src/main/java/nju/financecity_android/activity/ProductsActivity.java
+++ FinanceCity_Android/app/src/main/java/nju/financecity_android/activity/ProductsActivity.java
@@ -1,14 +1,26 @@
 package nju.financecity_android.activity;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import nju.financecity_android.R;
+import nju.financecity_android.widget.Footer;
 
 public class ProductsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_products);
+
+        loadComponent();
     }
+
+    private void loadComponent() {
+        footer = (Footer) findViewById(R.id.footer);
+    }
+
+    private Footer footer;
 }
---
+++ FinanceCity_Android/app/src/main/java/nju/financecity_android/widget/Banner.java
@@ -0,0 +1,51 @@
+package nju.financecity_android.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import nju.financecity_android.R;
+
+/**
+ * Created by coral on 16-8-22.
+ */
+public class Banner extends LinearLayout {
+
+    public Banner(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public Banner(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    //----------------------Interfaces------------------------
+
+    public String getDisplayString() {
+        return lblText.getText().toString();
+    }
+
+    public void setDisplayText(String displayText) {
+        lblText.setText(displayText);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        loadComponents();
+    }
+
+    private void loadComponents() {
+        lblText = (TextView) findViewById(R.id.lblText);
+        mainPane = (LinearLayout) findViewById(R.id.mainPane);
+    }
+
+    private void init(Context context) {
+        LayoutInflater.from(context).inflate(R.layout.banner, this, true);
+    }
+    private TextView lblText;
+    private LinearLayout mainPane;
+}
