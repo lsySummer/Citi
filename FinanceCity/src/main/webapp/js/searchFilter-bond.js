9cbd855b199ba2158a968eb749821792e8971e31
yzy14 <yzy14@software.nju.edu.cn>
2016-08-30 21:18:26 +0800

    searchFilter相关

---
+++ FinanceCity/src/main/webapp/js/searchFilter-bond.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Hermit on 16/8/30.
+ */
+window.onload = function() {
+    initSlider();
+};
+
+function initSlider() {
+    $("#annualized_return").slider({});
+    $("#range").slider({});
+}
