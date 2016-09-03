70562acc84ea9e409e0ad2ffe2ea60a946948b1c
sh14 <sh14@software.nju.edu.cn>
2016-09-04 03:02:34 +0800

    安卓: 1. 吐血添加了基金、银行理财、债券的产品详细信息界面

---
+++ FinanceCity_Android/app/src/main/java/nju/financecity_android/controller/activity/BondDetailActivity.java
@@ -0,0 +1,138 @@
+package nju.financecity_android.controller.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import nju.financecity_android.R;
+import nju.financecity_android.controller.widget.SimpleProperty;
+import nju.financecity_android.vo.PropertyVO;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class BondDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bond_detail);
+        initComponents();
+    }
+
+    protected void setHeaderInfo(String productName, String state, String length, String interestRate) {
+        txtPrdtName.setText(productName);
+        txtState.setText(state);
+        txtLength.setText(length);
+        txtInterestRate.setText(interestRate);
+    }
+
+    protected void processData(Map data) {
+        String productName = data.get("债权名称").toString();
+        String state = data.get("状态").toString();
+        String interestRate = data.get("票面利率").toString();
+        String length = data.get("期限").toString();
+        setHeaderInfo(productName, state, length, interestRate);
+
+        List<PropertyVO> properties = new ArrayList<>();
+        if (data.get("债券分类").toString().equals("储蓄式")) {
+            Collections.addAll(properties,
+                    new PropertyVO("基本信息", "", R.color.validBlue, 0),
+                    new PropertyVO("债券名称", productName),
+                    new PropertyVO("债券简称", data.get("债券简称")),
+                    new PropertyVO("债券代码", data.get("债券代码")),
+                    new PropertyVO("债券分类", data.get("债券分类")),
+                    new PropertyVO("发型单位", data.get("发型单位")),
+                    new PropertyVO("", ""),
+
+                    new PropertyVO("风险收益", "", R.color.validBlue, 0),
+                    new PropertyVO("票面利率", data.get("票面利率")),
+                    new PropertyVO("调整后年利率", data.get("调整后年利率")),
+                    new PropertyVO("计息、付息方式", data.get("计息、付息方式")),
+                    new PropertyVO("付息频率", data.get("付息频率")),
+                    new PropertyVO("", ""),
+
+                    new PropertyVO("申购赎回", "", R.color.validBlue, 0),
+                    new PropertyVO("期限", data.get("期限")),
+                    new PropertyVO("发行起始日", data.get("发行起始日")),
+                    new PropertyVO("发行截止日", data.get("发行截止日")),
+                    new PropertyVO("状态", data.get("状态")),
+                    new PropertyVO("到期日", data.get("到期日")),
+                    new PropertyVO("起息日", data.get("起息日")),
+                    new PropertyVO("提前兑取开始日期", data.get("提前兑取开始日期")),
+                    new PropertyVO("提前兑取计息开始日期", data.get("提前兑取计息开始日期")),
+                    new PropertyVO("", ""),
+
+                    new PropertyVO("其他", "", R.color.validBlue, 0),
+                    new PropertyVO("面值", data.get("面值")),
+                    new PropertyVO("发行价", data.get("发行价")));
+            setProperties(properties);
+        }
+        else {
+            Collections.addAll(properties,
+                    new PropertyVO("基本信息", "", R.color.validBlue, 0),
+                    new PropertyVO("债券名称", productName),
+                    new PropertyVO("债券简称", data.get("债券简称")),
+                    new PropertyVO("债券代码", data.get("债券代码")),
+                    new PropertyVO("债券分类", data.get("债券分类")),
+                    new PropertyVO("发型单位", data.get("发型单位")),
+                    new PropertyVO("", ""),
+
+                    new PropertyVO("风险收益", "", R.color.validBlue, 0),
+                    new PropertyVO("票面利率", data.get("票面利率")),
+                    new PropertyVO("调整后年利率", data.get("调整后年利率")),
+                    new PropertyVO("计息、付息方式", data.get("计息、付息方式")),
+                    new PropertyVO("付息频率", data.get("付息频率")),
+                    new PropertyVO("", ""),
+
+                    new PropertyVO("申购赎回", "", R.color.validBlue, 0),
+                    new PropertyVO("期限", data.get("期限")),
+                    new PropertyVO("发行起始日", data.get("发行起始日")),
+                    new PropertyVO("发行截止日", data.get("发行截止日")),
+                    new PropertyVO("状态", data.get("状态")),
+                    new PropertyVO("到期日", data.get("到期日")),
+                    new PropertyVO("起息日", data.get("起息日")),
+                    new PropertyVO("", ""),
+
+                    new PropertyVO("其他", "", R.color.validBlue, 0),
+                    new PropertyVO("面值", data.get("面值")),
+                    new PropertyVO("发行价", data.get("发行价")),
+                    new PropertyVO("上市地", data.get("上市地")),
+                    new PropertyVO("发布时间", data.get("发布时间")),
+                    new PropertyVO("上市流通日", data.get("上市流通日")),
+                    new PropertyVO("发行额", data.get("发行额")),
+                    new PropertyVO("兑讨价", data.get("兑讨价")),
+                    new PropertyVO("认购对象", data.get("认购对象")),
+                    new PropertyVO("债券价值", data.get("债券价值")),
+                    new PropertyVO("信用级别", data.get("信用级别")),
+                    new PropertyVO("发行方式", data.get("发行方式")),
+                    new PropertyVO("发行对象", data.get("发行对象")),
+                    new PropertyVO("主承销机构", data.get("主承销机构")),
+                    new PropertyVO("税收状况", data.get("税收状况")));
+            setProperties(properties);
+        }
+    }
+
+    protected void setProperties(List<PropertyVO> properties) {
+        SimpleProperty.PropertyListAdapter adapter = new SimpleProperty.PropertyListAdapter(this, properties);
+        listProperties.setAdapter(adapter);
+    }
+
+    private void initComponents() {
+        mInflater = LayoutInflater.from(this);
+
+        listProperties = (ListView) findViewById(R.id.listProperties);
+        txtInterestRate = (TextView) findViewById(R.id.txtInterestRate);
+        txtPrdtName = (TextView) findViewById(R.id.txtPdtName);
+        txtState = (TextView) findViewById(R.id.txtState);
+        txtLength = (TextView) findViewById(R.id.txtLength);
+    }
+
+    ListView listProperties;
+    TextView txtInterestRate, txtPrdtName, txtState, txtLength;
+    LayoutInflater mInflater;
+}
--- FinanceCity_Android/app/src/main/java/nju/financecity_android/controller/activity/FundDetailActivity.java
+++ FinanceCity_Android/app/src/main/java/nju/financecity_android/controller/activity/FundDetailActivity.java
@@ -1,6 +1,5 @@
 package nju.financecity_android.controller.activity;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
@@ -11,6 +10,7 @@ import nju.financecity_android.vo.PropertyVO;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 public class FundDetailActivity extends AppCompatActivity {
 
@@ -32,61 +32,60 @@ public class FundDetailActivity extends AppCompatActivity {
         txtState.setText(state);
     }
 
-    private void processData(Intent intent) {
-        String productName = intent.getStringExtra("productName");
-        double interestRate = intent.getDoubleExtra("interestRate", 0);
-        int netValue = intent.getIntExtra("netValue", 0);
-        String state = intent.getStringExtra("state");
+    private void processData(Map data) {
+        String productName = data.get("法定名称").toString();
+        double interestRate = Double.parseDouble(data.get("近一年收益率").toString());
+        int netValue = Integer.parseInt(data.get("最新净值").toString());
+        String state = data.get("状态").toString();
         setHeaderInfo(productName, interestRate, netValue, state);
 
         List<PropertyVO> properties = new ArrayList<>();
         Collections.addAll(properties,
                 new PropertyVO("基本信息", "", R.color.validBlue, 0),
                 new PropertyVO("法定名称", productName),
-                new PropertyVO("基金编号", intent.getStringExtra("基金编号")),
-                new PropertyVO("管理机构", intent.getStringExtra("管理机构")),
-                new PropertyVO("托管机构", intent.getStringExtra("托管机构")),
-                new PropertyVO("", intent.getStringExtra("")),
+                new PropertyVO("基金编号", data.get("基金编号")),
+                new PropertyVO("管理机构", data.get("管理机构")),
+                new PropertyVO("托管机构", data.get("托管机构")),
+                new PropertyVO("", data.get("")),
 
                 new PropertyVO("风险收益", "", R.color.validBlue, 0),
-                new PropertyVO("近一年收益率", intent.getStringExtra("近一年收益率")),
-                new PropertyVO("最新净值", intent.getStringExtra("最新净值")),
-                new PropertyVO("份额净值", intent.getStringExtra("份额净值")),
-                new PropertyVO("基金投资目标类型", intent.getStringExtra("基金投资目标类型")),
-                new PropertyVO("业绩比较基准", intent.getStringExtra("业绩比较基准")),
-                new PropertyVO("跟踪标的", intent.getStringExtra("跟踪标的")),
-                new PropertyVO("风险等级", intent.getStringExtra("风险等级")),
-                new PropertyVO("", intent.getStringExtra("")),
+                new PropertyVO("近一年收益率", data.get("近一年收益率")),
+                new PropertyVO("最新净值", data.get("最新净值")),
+                new PropertyVO("基金投资目标类型", data.get("基金投资目标类型")),
+                new PropertyVO("业绩比较基准", data.get("业绩比较基准")),
+                new PropertyVO("跟踪标的", data.get("跟踪标的")),
+                new PropertyVO("风险等级", data.get("风险等级")),
+                new PropertyVO("", data.get("")),
 
                 new PropertyVO("申购赎回", "", R.color.validBlue, 0),
-                new PropertyVO("状态", intent.getStringExtra("状态")),
-                new PropertyVO("是否封闭", intent.getStringExtra("是否封闭")),
-                new PropertyVO("申购费率", intent.getStringExtra("申购费率")),
-                new PropertyVO("认购费率", intent.getStringExtra("认购费率")),
-                new PropertyVO("赎回费率", intent.getStringExtra("赎回费率")),
-                new PropertyVO("广义管理费率", intent.getStringExtra("广义管理费率")),
-                new PropertyVO("递增购买最小单位", intent.getStringExtra("递增购买最小单位")),
-                new PropertyVO("募集开始日", intent.getStringExtra("募集开始日")),
-                new PropertyVO("募集截止日", intent.getStringExtra("募集截止日")),
-                new PropertyVO("起息日", intent.getStringExtra("起息日")),
-                new PropertyVO("开发日", intent.getStringExtra("开发日")),
-                new PropertyVO("期限", intent.getStringExtra("期限")),
-                new PropertyVO("认购速度", intent.getStringExtra("认购速度")),
-                new PropertyVO("赎回速度", intent.getStringExtra("赎回速度")),
-                new PropertyVO("", intent.getStringExtra("")),
+                new PropertyVO("状态", data.get("状态")),
+                new PropertyVO("是否封闭", data.get("是否封闭")),
+                new PropertyVO("申购费率", data.get("申购费率")),
+                new PropertyVO("认购费率", data.get("认购费率")),
+                new PropertyVO("赎回费率", data.get("赎回费率")),
+                new PropertyVO("广义管理费率", data.get("广义管理费率")),
+                new PropertyVO("递增购买最小单位", data.get("递增购买最小单位")),
+                new PropertyVO("募集开始日", data.get("募集开始日")),
+                new PropertyVO("募集截止日", data.get("募集截止日")),
+                new PropertyVO("起息日", data.get("起息日")),
+                new PropertyVO("开发日", data.get("开发日")),
+                new PropertyVO("期限", data.get("期限")),
+                new PropertyVO("认购速度", data.get("认购速度")),
+                new PropertyVO("赎回速度", data.get("赎回速度")),
+                new PropertyVO("", data.get("")),
 
 
                 new PropertyVO("其他", "", R.color.validBlue, 0),
-                new PropertyVO("基金经理", intent.getStringExtra("基金经理")),
-                new PropertyVO("基金规模", intent.getStringExtra("基金规模")),
-                new PropertyVO("份额规模", intent.getStringExtra("份额规模")));
+                new PropertyVO("基金经理", data.get("基金经理")),
+                new PropertyVO("基金规模", data.get("基金规模")),
+                new PropertyVO("份额规模", data.get("份额规模")));
     }
 
     private void initComponents() {
         txtInterestRate = (TextView) findViewById(R.id.txtInterestRate);
         txtPrdtName = (TextView) findViewById(R.id.txtPdtName);
         txtNewestNetValue = (TextView) findViewById(R.id.txtNewestNetValue);
-        txtState = (TextView) findViewById(R.id.txtState);
+        txtState = (TextView) findViewById(R.id.txtLength);
         listProperties = (ListView) findViewById(R.id.listProperties);
     }
 
