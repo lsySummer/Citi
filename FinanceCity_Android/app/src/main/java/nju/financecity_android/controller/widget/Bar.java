bc5483383b68d2da18d57ff5966ae357e9a1200f
yfraquelle <y.f.raquelle@gmail.com>
2016-09-04 05:10:26 +0800

    widget layout and fragment

--- FinanceCity_Android/app/src/main/java/nju/financecity_android/controller/widget/Bar.java
+++ FinanceCity_Android/app/src/main/java/nju/financecity_android/controller/widget/Bar.java
@@ -1,7 +1,9 @@
 package nju.financecity_android.controller.widget;
 
 import android.content.Context;
+import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.widget.LinearLayout;
@@ -19,17 +21,43 @@ public class Bar extends LinearLayout {
 	
     public Bar(Context context) {
         super(context);
+        //加载视图的布局
         LayoutInflater.from(context).inflate(R.layout.bar, this, true);
+        onFinishInflate();
     }
 
     public Bar(Context context, AttributeSet attrs) {
         super(context, attrs);
+        //加载视图的布局
         LayoutInflater.from(context).inflate(R.layout.bar, this, true);
+        onFinishInflate();
+        //加载自定义的属性
+//        TypedArray a=context.obtainStyledAttributes(attrs,R.styleable.Header);
+//        titleText=a.getString(R.styleable.Header_titleText);
+//        titleTextColor=a.getColor(R.styleable.Header_titleTextColor, Color.WHITE);
+//        titleTextSize=a.getDimension(R.styleable.Header_titleTextSize,20f);
+
+        //回收资源，这一句必须调用
+//        a.recycle();
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        //获取子控件
         bar_text = (TextView) findViewById(R.id.bar_text);
         bar_rangebar= (RangeBar) findViewById(R.id.bar_rangebar);
         start=bar_rangebar.getTickStart();
         end=bar_rangebar.getTickEnd();
         interval=bar_rangebar.getTickInterval();
+
+        //将从资源文件中加载的属性设置给子控件
+//        if (!TextUtils.isEmpty(titleText))
+//            setPageTitleText(titleText);
+//        setPageTitleTextColor(titleTextColor);
+//        setPageTitleTextSize(titleTextSize);
+
     }
 
     public String getBar_text() {
