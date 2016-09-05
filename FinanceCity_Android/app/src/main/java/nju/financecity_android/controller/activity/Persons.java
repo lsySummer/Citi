c8043a3f8aeb70f5ab02f88a632b2b62ae7b9eea
Sam <rww14@software.nju.edu.cn>
2016-09-06 01:16:05 +0800

    安卓：个人信息

--- FinanceCity_Android/app/src/main/java/nju/financecity_android/controller/activity/Persons.java
+++ FinanceCity_Android/app/src/main/java/nju/financecity_android/controller/activity/Persons.java
@@ -1,11 +1,14 @@
 package nju.financecity_android.controller.activity;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import nju.financecity_android.R;
 
@@ -13,6 +16,16 @@ import nju.financecity_android.R;
  * Created by sam on 16/9/5.
  */
 public class Persons extends Fragment {
+
+    private TextView My_Name;
+    private TextView Mobile;
+    private TextView Birthday;
+    private TextView IfCity;
+    private TextView GetIn;
+    private TextView GetOut;
+    private Button Modify;
+
+    private View theview;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -22,5 +35,34 @@ public class Persons extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
+        theview = getView();
+        initComponent();
+        addListener();
+    }
+
+    private void initComponent(){
+        My_Name = (TextView) findViewById(R.id.id_name);
+        Mobile = (TextView) findViewById(R.id.mobile);
+        Birthday = (TextView) findViewById(R.id.birthday);
+        IfCity = (TextView) findViewById(R.id.city_country);
+        GetIn = (TextView) findViewById(R.id.get_in);
+        GetOut = (TextView) findViewById(R.id.get_out);
+        Modify = (Button) findViewById(R.id.modify_mes);
+    }
+
+    private View findViewById(int ResId){
+        View FindView = theview.findViewById(ResId);
+        return FindView;
+    }
+
+    private void addListener(){
+        Modify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),Persons_Edit.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
