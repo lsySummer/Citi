0018d313d24d275bb3f6bde8a348769e24d2cb5d
Sorumi <Sorumi33@gmail.com>
2016-09-14 22:49:34 +0800

    雷达图的修改

--- FinanceCity/src/main/webapp/js/recommendation.js
+++ FinanceCity/src/main/webapp/js/recommendation.js
@@ -14,7 +14,6 @@ window.onload = function() {
 
 function  initPrecentChart(chartId) {
     zingchart.MODULESDIR = "https://cdn.zingchart.com/modules/";
-
     zingchart.THEME="classic";
 
     var myConfig = {
@@ -94,44 +93,111 @@ function  initPrecentChart(chartId) {
 
 
 function initRadarChart(chartId) {
-    var chart = AmCharts.makeChart( chartId, {
+    zingchart.MODULESDIR = "https://cdn.zingchart.com/modules/"
+    zingchart.THEME="classic";
+    var myConfig =
+    {
         "type": "radar",
-        "theme": "light",
-        "dataProvider": [ {
-            "country": "美不美债券",
-            "litres": 156.9
-        }, {
-            "country": "稳赚利38天",
-            "litres": 131.1
-        }, {
-            "country": "大长腿保险",
-            "litres": 115.8
-        }, {
-            "country": "什么理财",
-            "litres": 109.9
-        }, {
-            "country": "一个基金",
-            "litres": 108.3
-            //}, {
-            //    "country": "UK",
-            //"litres": 99
-        } ],
-        "valueAxes": [ {
-            "axisTitleOffset": 20,
-            "minimum": 0,
-            "axisAlpha": 0.15
-        } ],
-        "startDuration": 2,
-        "graphs": [ {
-            "balloonText": "[[value]] litres of beer per year",
-            "bullet": "round",
-            "lineThickness": 2,
-            "valueField": "litres"
-        } ],
-        "categoryField": "country",
-        "export": {
-            "enabled": false
-        }
-    } );
+        "plot": {
+            "aspect": "line"
+        },
+        "background-color": "Transparent",
+
+        "title": {
+            "background-color": "none",
+            "font-color": "000",
+            //"font-size": "22px"
+        },
+        "tooltip": {
+            //"text": "%t<br>%k Is %v",
+            "shadow": 0,
+            "border-radius": 3
+        },
+        "scale-k": {
+            "background-color": "none",
+            "values": [
+                "收益性",
+                "流动性",
+                "平均期限",
+                "风险性",
+            ],
+            "item": {
+                "font-color":"#808080",
+                "font-weight":"100",
+                "font-size": "10px",
+                "padding-left": "5px",
+                "padding-bottom": "5px"
+            },
+            "guide": {
+                "line-color": "",
+                "line-style": "solid",
+                "line-width": "2px",
+                "items": [
+                    {
+                        "background-color": "#fff"
+                    }
+                ]
+            },
+            "tick": {
+                "visible": false
+            }
+        },
+        "scale-v": {
+            "values": [
+                20,40,60,80,100
+            ],
+            "item": {
+                "font-color":"Transparent",
+                "padding-left": "0px",
+                "font-size": "10px"
+            },
+            "ref-line": {
+                "line-color": ""
+            },
+
+            "tick": {
+                "line-color": ""
+            }
+        },
+        "series": [
+            {
+                "values": [
+                    90,70,40,50
+                ],
+                "aspect": "line",
+                "text": "ER",
+                "line-color": "#11b7f3",
+                "background-color": "none",
+                "line-width": "2px",
+                "alpha": "1",
+                "marker": {
+                    "background-color": "#11b7f3",
+                    "size": "3",
+                    "border-color": "#11b7f3",
+                    "alpha": "1"
+                }
+            },
+            {
+                "aspect": "line",
+                "text": "ENT",
+                "line-color": "#11b7f3",
+                "line-width": "4px",
+                "alpha": "0",
+                "marker": {
+                    "background-color": "#666666",
+                    "size": "4",
+                    "border-color": "#666666",
+                    "alpha": "0.55"
+                }
+            }
+        ]
+    };
+
+    zingchart.render({
+        id : chartId,
+        data : myConfig,
+        height: 300,
+        width: 300
+    });
 }
 
